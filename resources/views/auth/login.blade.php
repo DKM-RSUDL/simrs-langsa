586646d34f8cbaee4287c228b6b84d0f3a2dcd5f
hayuraaa <haris200170154@mhs.unimal.ac.id>
2024-09-17 15:50:13 +0700

    page-login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,98 +1,73 @@
 @extends('layouts.auth.app')
 
 @section('content')
-    <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
-        <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
-        <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+        <div class="row justify-content-sm-center h-100 align-items-center">
+        <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-7 col-sm-8">
+            <div class="card shadow-lg">
+                <div class="card-body p-4">
+                    <h1 class="fs-4 text-center fw-bold mb-4">{{ __('Login') }}</h1>
+                    <h1 class="fs-6 mb-3">Please enter your email and password to log in.</h1>
 
-        <div class="card bg-glass">
-            <div class="card-body px-7 py-5 px-md-4">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="row">
-                        <div class="col-md-8 mb-2 mx-auto">
-                            <p1>Welcome!</p1>
-                            <br>
-                            <p style="color:#245953">
-                                Please log in your correct credentials.
-                            </p>
-                            <!-- Email input -->
-                            <div class="form-outline mb-3">
-                                <input id="email" type="email"
-                                    class="form-control @error('email') is-invalid @enderror" name="email"
-                                    value="{{ old('email') }}" placeholder="Email Address" autocomplete="email" autofocus
-                                    @required(true)>
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label class="mb-2 text-muted" for="email">{{ __('Email Address') }}</label>
+                            <div class="input-group input-group-join mb-3">
+                                <input id="email" type="email" placeholder="Enter Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <span class="input-group-text rounded-end" role="alert">&nbsp<i class="fa fa-envelope"></i>&nbsp</span>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                 @enderror
                             </div>
-                            <!-- Password input -->
-                            <div class="form-outline mb-1">
-                                <div class="input-group mb-3">
-                                    <input id="password" type="password"
-                                        class="form-control show-password rounded @error('password') is-invalid @enderror"
-                                        name="password" placeholder="Password" autocomplete="current-password"
-                                        @required(true)>
-                                    <span class="input-group-text rounded bg-white ml-1 toggle-password">
-                                        <a href="javascript:void(0)" class="text-dark">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                    </span>
-                                </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <div class="mb-2 w-100">
+                                <label class="text-muted" for="password">{{ __('Password') }}</label>
+                            </div>
+                            <div class="input-group input-group-join mb-3">
+                                <input id="password" type="password" placeholder="Your password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <span class="input-group-text rounded-end password cursor-pointer">&nbsp<i class="fa fa-eye"></i>&nbsp</span>
+
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                 @enderror
                             </div>
+                        </div>
 
-                            <!-- Remember me -->
-                            <div class="form-group row">
-                                <div class="col-md-6">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="remember">
-                                            <small>
-                                                Remember Me
-                                            </small>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-check">
-                                        <a href="{{ route('password.request') }}">
-                                            <small>
-                                                Forgot Password?
-                                            </small>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <!-- Submit button -->
-                            <div class="d-flex justify-content-around align-items-center mb-2">
-                                <button type="submit" class="btn btn-success btn-block">
-                                    Log In
-                                </button>
-                            </div>
-
-                            <!-- register -->
-                            <div class="form-group row">
-                                <div class="col-md-12">
-                                    <div class="form-check">
-                                        <a href="{{ route('register') }}">
-                                            Don't Have An Account?
-                                        </a>
-                                    </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-0">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+
+                        <div class="d-flex align-items-center">
+                            <button type="submit" class="btn btn-primary ms-auto">
+                                 {{ __('Login') }}
+                            </button>
+
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="text-center mt-5 text-muted">
+                Copyright &copy; 2024 &mdash; SIMRS Langsa
             </div>
         </div>
     </div>
--- resources/views/layouts/auth/app.blade copy.php
+++ resources/views/layouts/auth/app.blade copy.php
@@ -1,22 +1,19 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Authentication Login &mdash; Arfa</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-        crossorigin="anonymous" />
-
-
-    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <!-- <link rel="stylesheet" href="../vendor/themify-icons/themify-icons.css"> -->
-    <link rel="stylesheet" href="../assets/css/bootstrap-override.css">
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Laravel 10')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite(['resources/css/auth.css'])
 
 </head>
 
@@ -24,6 +21,8 @@
     <section class="login">
         <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
             <div class="row gx-lg-5 align-items-center mb-5">
+
+                @include('layouts.auth.left-side')
 
                 @yield('content')
 
