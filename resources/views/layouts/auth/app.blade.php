e835672ee9bb155ce6d4d7916910eda57db42dc9
hayuraaa <haris200170154@mhs.unimal.ac.id>
2024-09-17 16:34:55 +0700

    css

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
-
+    @vite(['resources/css/auth.css'])
 
     <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
 
