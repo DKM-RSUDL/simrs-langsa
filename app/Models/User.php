6f5ecce01aabc08acc80f0af8ba4f8dae2d062c5
hayuraaa <haris200170154@mhs.unimal.ac.id>
2024-09-18 16:22:58 +0700

    dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    protected $table = 'b_users';
+
     /**
      * The attributes that are mass assignable.
      *
