0003e50ff6964031881672592fa9da1377ce8d62
NasDev07 <nasruddin20182019@gmail.com>
2024-09-23 10:04:17 +0700

    data gawat darurat

--- app/Http/Controllers/UnitPelayanan/GawatDaruratController.php
+++ app/Http/Controllers/UnitPelayanan/GawatDaruratController.php
@@ -6,16 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Kunjungan;
 use App\Models\TestModel;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class GawatDaruratController extends Controller
 {
     protected $roleService;
     public function index(Request $request)
-    {
-        $DataKunjungan = Kunjungan::all();
-        if ($request->ajax()) {
-            return $this->roleService->dataTable();
-        }
-        return view('unit-pelayanan.gawat-darurat.index', compact('DataKunjungan'));
+{
+    if ($request->ajax()) {
+        $data = Kunjungan::select(['kd_pasien', 'kd_dokter', 'tgl_masuk']); // Pilih kolom yang diperlukan
+        return DataTables::of($data)
+            ->addColumn('action', function($row){
+                $btn = '<a href="" class="edit btn btn-secondary btn-sm"><i class="ti-pencil-alt"></i></a>';
+                $btn .= '<a href="" class="btn btn-secondary btn-sm">...</a>';
+                return $btn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
     }
+    return view('unit-pelayanan.gawat-darurat.index');
+}
 }
