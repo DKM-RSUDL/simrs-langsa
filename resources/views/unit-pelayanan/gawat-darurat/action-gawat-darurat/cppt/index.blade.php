aed782ef5a51f83268f0d7d2b3c9a23694421a21
NasDev07 <nasruddin20182019@gmail.com>
2024-09-26 16:24:47 +0700

    membuat page cppt dan modifikasi

--- resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/cppt/index.blade.php
+++ resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/cppt/index.blade.php
@@ -300,10 +300,53 @@
 
 @push('js')
 <script>
-    // Event listener untuk membuka Modal Kedua tanpa menutup Modal Pertama
-    document.getElementById('openModalKedua').addEventListener('click', function() {
-        var modalKedua = new bootstrap.Modal(document.getElementById('verticalCenter'));
-        modalKedua.show();
+    document.addEventListener('DOMContentLoaded', function () {
+        const searchInput = document.getElementById('searchInput');
+        const dataList = document.getElementById('dataList');
+        const items = dataList.querySelectorAll('.dropdown-item');
+
+        // dropdown saat input di klik
+        searchInput.addEventListener('focus', function () {
+            dataList.classList.add('show');
+        });
+
+        // dropdown user mulai mengetik
+        searchInput.addEventListener('input', function () {
+            const filter = searchInput.value.toLowerCase();
+            let hasVisibleItems = false;
+
+            items.forEach(function (item) {
+                const text = item.textContent.toLowerCase();
+                if (text.includes(filter)) {
+                    item.style.display = 'block';
+                    hasVisibleItems = true;
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+
+            // Jika tidak ada item yang cocok, tutup dropdown
+            if (hasVisibleItems) {
+                dataList.classList.add('show');
+            } else {
+                dataList.classList.remove('show');
+            }
+        });
+
+        // Event listener untuk klik pada item dropdown
+        items.forEach(function (item) {
+            item.addEventListener('click', function () {
+                searchInput.value = this.textContent;
+                dataList.classList.remove('show');
+            });
+        });
+
+        // Menyembunyikan dropdown saat klik di luar
+        document.addEventListener('click', function (event) {
+            if (!event.target.closest('.dropdown')) {
+                dataList.classList.remove('show');
+            }
+        });
     });
 </script>
 
