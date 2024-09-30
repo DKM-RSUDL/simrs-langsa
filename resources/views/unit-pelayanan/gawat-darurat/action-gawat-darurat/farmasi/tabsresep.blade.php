ada132e52d5f80cb195b6a906bdf1f3e36917e84
NasDev07 <nasruddin20182019@gmail.com>
2024-09-30 11:12:24 +0700

    Menyimpan perubahan labor

--- resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/farmasi/tabsresep.blade.php
+++ resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/farmasi/tabsresep.blade.php
@@ -3,7 +3,7 @@
 <div>
     {{-- Filter Tabel: Episode, Tanggal Pemberian Obat, Radio Button, dan Tombol Tambah --}}
     <div class="d-flex justify-content-between align-items-center m-3">
-        
+
         <!-- Wrapper untuk Tanggal dan Radio Button di Sebelah Kiri -->
         <div class="d-flex align-items-center">
             <!-- Tanggal Pemberian Obat -->
@@ -14,13 +14,15 @@
             <!-- Radio Button untuk status obat -->
             <div class="d-flex align-items-center">
                 <div class="form-check me-3">
-                    <input class="form-check-input" type="radio" name="status_obat" id="diberikan_obat" value="diberikan" checked>
+                    <input class="form-check-input" type="radio" name="status_obat" id="diberikan_obat" value="diberikan"
+                        checked>
                     <label class="form-check-label" for="diberikan_obat">
-                        Diberikan Obat
+                        Diberikan Obat -tes
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="status_obat" id="tidak_diberikan_obat" value="tidak_diberikan">
+                    <input class="form-check-input" type="radio" name="status_obat" id="tidak_diberikan_obat"
+                        value="tidak_diberikan">
                     <label class="form-check-label" for="tidak_diberikan_obat">
                         Tidak Diberikan Obat
                     </label>
@@ -30,7 +32,7 @@
 
         <!-- Tombol "Tambah" di Sebelah Kanan -->
         <div>
-            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailPasienModal" type="button">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahResep" type="button">
                 <i class="ti-plus"></i> Tambah
             </button>
         </div>
@@ -57,3 +59,4 @@
         </tbody>
     </table>
 </div>
+@include('unit-pelayanan.gawat-darurat.action-gawat-darurat.farmasi.modalresep')
