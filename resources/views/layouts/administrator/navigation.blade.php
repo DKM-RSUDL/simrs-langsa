557fc40514b7e581d199917312aa243d77ac4a5b
NasDev07 <nasruddin20182019@gmail.com>
2024-10-30 09:52:21 +0700

    perbaiki menu navigasi active dan no active

--- resources/views/layouts/administrator/navigation.blade.php
+++ resources/views/layouts/administrator/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="main-sidebar ps-menu">
+{{-- <nav class="main-sidebar ps-menu">
     <div class="sidebar-header">
         <div class="text">Administrator</div>
         <div class="close-sidebar action-toggle">
@@ -42,6 +42,60 @@
                         </li>
                     @elseif ($menu->type_menu == 'single')
                         <li class="{{ request()->segment(1) == $menu->url ? 'active' : '' }}">
+                            <a href="{{ url($menu->url) }}" class="link">
+                                <i class="{{ $menu->icon }}"></i>
+                                <span class="text-capitalize">{{ $menu->name }}</span>
+                            </a>
+                        </li>
+                    @endif
+                @endcan
+            @endforeach
+        </ul>
+    </div>
+</nav> --}}
+
+<!-- Pembaruan menu active dari anas -->
+<nav class="main-sidebar ps-menu">
+    <div class="sidebar-header">
+        <div class="text">Administrator</div>
+        <div class="close-sidebar action-toggle">
+            <i class="ti-close"></i>
+        </div>
+    </div>
+    <div class="sidebar-content">
+        <ul>
+            <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                <a href="{{ route('home') }}" class="link">
+                    <i class="fa-solid fa-home"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="menu-category">
+                <span class="text-uppercase">User Interface</span>
+            </li>
+
+            @foreach (getMenus() as $menu)
+                @can('read ' . $menu->url)
+                    @if ($menu->type_menu == 'parent')
+                        <li class="{{ collect($menu->subMenus)->contains(function($submenu) { return request()->is($submenu->url); }) ? 'active open' : '' }}">
+                            <a href="#" class="main-menu has-dropdown">
+                                <i class="{{ $menu->icon }}"></i>
+                                <span class="text-capitalize">{{ $menu->name }}</span>
+                            </a>
+                            <ul class="sub-menu {{ collect($menu->subMenus)->contains(function($submenu) { return request()->is($submenu->url); }) ? 'expand' : '' }}">
+                                @foreach ($menu->subMenus as $submenu)
+                                    @can('read ' . $submenu->url)
+                                        <li class="{{ request()->is($submenu->url) ? 'active' : '' }}">
+                                            <a href="{{ url($submenu->url) }}" class="link">
+                                                <span class="text-capitalize">{{ $submenu->name }}</span>
+                                            </a>
+                                        </li>
+                                    @endcan
+                                @endforeach
+                            </ul>
+                        </li>
+                    @elseif ($menu->type_menu == 'single')
+                        <li class="{{ request()->is($menu->url) ? 'active' : '' }}">
                             <a href="{{ url($menu->url) }}" class="link">
                                 <i class="{{ $menu->icon }}"></i>
                                 <span class="text-capitalize">{{ $menu->name }}</span>
