8255278fa7a5686a98e82438a6dbe66cc3a295bc
NasDev07 <nasruddin20182019@gmail.com>
2024-12-12 09:35:49 +0700

    Ranap

--- app/Http/Controllers/UnitPelayanan/RawatInap/AsuhanKeperawatanRawatInapController.php
+++ app/Http/Controllers/UnitPelayanan/RawatInap/AsuhanKeperawatanRawatInapController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers\UnitPelayanan\RawatInap;
 
 use App\Http\Controllers\Controller;
+use App\Models\DokterInap;
+use App\Models\Kunjungan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class AsuhanKeperawatanRawatInapController extends Controller
 {
-    public function index()
+    public function index(Request $request, $kd_unit, $kd_pasien, $tgl_masuk, $urut_masuk)
     {
-        return view('unit-pelayanan.rawat-inap.pelayanan.asuhan-keperawatan.index');
+        $dataMedis = Kunjungan::with(['pasien', 'dokter', 'customer', 'unit'])
+            ->join('transaksi as t', function ($join) {
+                $join->on('kunjungan.kd_pasien', '=', 't.kd_pasien');
+                $join->on('kunjungan.kd_unit', '=', 't.kd_unit');
+                $join->on('kunjungan.tgl_masuk', '=', 't.tgl_transaksi');
+                $join->on('kunjungan.urut_masuk', '=', 't.urut_masuk');
+            })
+            ->where('kunjungan.kd_pasien', $kd_pasien)
+            ->where('kunjungan.kd_unit', $kd_unit)
+            ->where('kunjungan.urut_masuk', $urut_masuk)
+            ->whereDate('kunjungan.tgl_masuk', $tgl_masuk)
+            ->first();
+
+        $dataDokter = DokterInap::with(['dokter', 'unit'])
+            ->where('kd_unit', '1001')
+            ->whereRelation('dokter', 'status', 1)
+            ->get();
+
+        if ($dataMedis->pasien && $dataMedis->pasien->tgl_lahir) {
+            $dataMedis->pasien->umur = Carbon::parse($dataMedis->pasien->tgl_lahir)->age;
+        } else {
+            $dataMedis->pasien->umur = 'Tidak Diketahui';
+        }
+
+        if (!$dataMedis) {
+            abort(404, 'Data not found');
+        }
+        return view('unit-pelayanan.rawat-inap.pelayanan.asuhan-keperawatan.index', compact(
+            'dataMedis',
+            'dataDokter'
+        ));
     }
 }
--- resources/views/unit-pelayanan/rawat-inap/pelayanan/asuhan-keperawatan/index.blade.php
+++ resources/views/unit-pelayanan/rawat-inap/pelayanan/asuhan-keperawatan/index.blade.php
@@ -1,118 +1,137 @@
 @extends('layouts.administrator.master')
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/MedisGawatDaruratController.css') }}">
+    <style>
+
+    </style>
+@endpush
 
 @section('content')
-    <div class="d-flex justify-content-center">
-        <div class="w-100 h-100">
-            <div class="d-flex justify-content-between m-3">
-                <div class="row">
-                    <!-- Select Option -->
-                    <div class="col-md-2">
-                        <select class="form-select" id="SelectOption" aria-label="Pilih...">
-                            <option value="semua" selected>Semua Episode</option>
-                            <option value="option1">Episode Sekarang</option>
-                            <option value="option2">1 Bulan</option>
-                            <option value="option3">3 Bulan</option>
-                            <option value="option4">6 Bulan</option>
-                            <option value="option5">9 Bulan</option>
-                        </select>
-                    </div>
+    <div class="row">
+        <div class="col-md-3">
+            @include('components.patient-card')
+        </div>
 
-                    <!-- Start Date -->
-                    <div class="col-md-2">
-                        <input type="date" name="start_date" id="start_date" class="form-control"
-                            placeholder="Dari Tanggal">
-                    </div>
-
-                    <!-- End Date -->
-                    <div class="col-md-2">
-                        <input type="date" name="end_date" id="end_date" class="form-control" placeholder="S.d Tanggal">
-                    </div>
-                    <div class="col-md-1">
-                        <a href="#" class="btn btn-secondary rounded-3" id="filterButton"><i
-                                class="bi bi-funnel-fill"></i></a>
-                    </div>
-
-                    <!-- Search Bar -->
-                    <div class="col-md-3">
-                        <form method="GET" action="">
-
-                            <div class="input-group">
-                                <input type="text" name="search" class="form-control" placeholder="Cari..."
-                                    aria-label="Cari" value="" aria-describedby="basic-addon1">
-                                <button type="submit" class="btn btn-primary">Cari</button>
+        <div class="col-md-9">
+            @include('components.navigation-ranap')
+            <div class="d-flex justify-content-center">
+                <div class="w-100 h-100">
+                    <div class="d-flex justify-content-between m-3">
+                        <div class="row">
+                            <!-- Select Option -->
+                            <div class="col-md-2">
+                                <select class="form-select" id="SelectOption" aria-label="Pilih...">
+                                    <option value="semua" selected>Semua Episode</option>
+                                    <option value="option1">Episode Sekarang</option>
+                                    <option value="option2">1 Bulan</option>
+                                    <option value="option3">3 Bulan</option>
+                                    <option value="option4">6 Bulan</option>
+                                    <option value="option5">9 Bulan</option>
+                                </select>
                             </div>
-                        </form>
+
+                            <!-- Start Date -->
+                            <div class="col-md-2">
+                                <input type="date" name="start_date" id="start_date" class="form-control"
+                                    placeholder="Dari Tanggal">
+                            </div>
+
+                            <!-- End Date -->
+                            <div class="col-md-2">
+                                <input type="date" name="end_date" id="end_date" class="form-control"
+                                    placeholder="S.d Tanggal">
+                            </div>
+                            <div class="col-md-1">
+                                <a href="#" class="btn btn-secondary rounded-3" id="filterButton"><i
+                                        class="bi bi-funnel-fill"></i></a>
+                            </div>
+
+                            <!-- Search Bar -->
+                            <div class="col-md-3">
+                                <form method="GET" action="">
+
+                                    <div class="input-group">
+                                        <input type="text" name="search" class="form-control" placeholder="Cari..."
+                                            aria-label="Cari" value="" aria-describedby="basic-addon1">
+                                        <button type="submit" class="btn btn-primary">Cari</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="col-md-2">
+                                <a href="javascript:void(0)" class="btn btn-primary" id="btn-asuhan-create">
+                                    <i class="bi bi-plus-square"></i> Tambah
+                                </a>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="col-md-2">
-                        <a href="javascript:void(0)" class="btn btn-primary" id="btn-asuhan-create">
-                            <i class="bi bi-plus-square"></i> Tambah
-                        </a>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-sm table-hover">
+                            <thead class="table-primary">
+                                <tr>
+                                    <th>Tanggal</th>
+                                    <th>Waktu</th>
+                                    <th>Petugas</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>04 Des 2024</td>
+                                    <td>Pagi</td>
+                                    <td>Haris Yunanda</td>
+                                    <td>
+                                        <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
+                                            <i class="ti-eye"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" id="btn-asuhan-edit"
+                                            class="mb-2 btn btn-sm btn-warning">
+                                            <i class="ti-pencil"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
+                                            <i class="bi bi-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>05 Des 2024</td>
+                                    <td>Sore</td>
+                                    <td>Nasruddin</td>
+                                    <td>
+                                        <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
+                                            <i class="ti-eye"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" id="btn-asuhan-edit"
+                                            class="mb-2 btn btn-sm btn-warning">
+                                            <i class="ti-pencil"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
+                                            <i class="bi bi-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>06 Des 2024</td>
+                                    <td>Malam</td>
+                                    <td>Rizaldi</td>
+                                    <td>
+                                        <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
+                                            <i class="ti-eye"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" id="btn-asuhan-edit"
+                                            class="mb-2 btn btn-sm btn-warning">
+                                            <i class="ti-pencil"></i>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
+                                            <i class="bi bi-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </div>
-
-            <div class="table-responsive">
-                <table class="table table-bordered table-sm table-hover">
-                    <thead class="table-primary">
-                        <tr>
-                            <th>Tanggal</th>
-                            <th>Waktu</th>
-                            <th>Petugas</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>04 Des 2024</td>
-                            <td>Pagi</td>
-                            <td>Haris Yunanda</td>
-                            <td>
-                                <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
-                                    <i class="ti-eye"></i>
-                                </a>
-                                <a href="javascript:void(0)" id="btn-asuhan-edit" class="mb-2 btn btn-sm btn-warning">
-                                    <i class="ti-pencil"></i>
-                                </a>
-                                <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>05 Des 2024</td>
-                            <td>Sore</td>
-                            <td>Nasruddin</td>
-                            <td>
-                                <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
-                                    <i class="ti-eye"></i>
-                                </a>
-                                <a href="javascript:void(0)" id="btn-asuhan-edit" class="mb-2 btn btn-sm btn-warning">
-                                    <i class="ti-pencil"></i>
-                                </a>
-                                <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>06 Des 2024</td>
-                            <td>Malam</td>
-                            <td>Rizaldi</td>
-                            <td>
-                                <a href="javascript:void(0)" id="btn-asuhan-show" class="mb-2 btn btn-sm btn-info">
-                                    <i class="ti-eye"></i>
-                                </a>
-                                <a href="javascript:void(0)" id="btn-asuhan-edit" class="mb-2 btn btn-sm btn-warning">
-                                    <i class="ti-pencil"></i>
-                                </a>
-                                <a href="javascript:void(0)" class="mb-2 btn btn-sm btn-danger btn-delete">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
