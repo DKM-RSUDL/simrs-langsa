bf365793430886ffb23a71e55cb8cbdcc32b4b25
NasDev07 <nasruddin20182019@gmail.com>
2025-01-23 15:51:34 +0700

    matikan vite

--- resources/views/layouts/administrator/master.blade.php
+++ resources/views/layouts/administrator/master.blade.php
@@ -42,7 +42,12 @@
     {{-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script> --}}
 
     {{-- vite --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
 
 </head>
