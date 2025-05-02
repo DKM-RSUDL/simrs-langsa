4b25e1f41be016f30843ec460ede27149bba1c59
Nas.Dev <71059706+NasDev07@users.noreply.github.com>
2025-05-02 16:47:05 +0700

    IGD permintaan darah create

--- resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/permintaan-darah/index.blade.php
+++ resources/views/unit-pelayanan/gawat-darurat/action-gawat-darurat/permintaan-darah/index.blade.php
@@ -1,150 +1,129 @@
 @extends('layouts.administrator.master')
 @push('css')
-    <link rel="stylesheet" href="{{ asset('assets/css/MedisGawatDaruratController.css') }}">
-    <style>
+<link rel="stylesheet" href="{{ asset('assets/css/MedisGawatDaruratController.css') }}">
+<style>
 
-    </style>
+</style>
 @endpush
 
 @section('content')
-    <div class="row">
-        <div class="col-md-3">
-            @include('components.patient-card')
-        </div>
+<div class="row">
+    <div class="col-md-3">
+        @include('components.patient-card')
+    </div>
 
-        <div class="col-md-9">
-            @include('components.navigation')
+    <div class="col-md-9">
 
-            <div class="row">
-                <div class="d-flex justify-content-between m-3">
-                    <div class="row">
-                        <!-- Start Date -->
-                        <div class="col-md-2">
-                            <input type="date" name="start_date" id="start_date" class="form-control"
-                                placeholder="Dari Tanggal">
-                        </div>
-
-                        <!-- End Date -->
-                        <div class="col-md-2">
-                            <input type="date" name="end_date" id="end_date" class="form-control"
-                                placeholder="S.d Tanggal">
-                        </div>
-                        <div class="col-md-2">
-                            <a href="#" class="btn btn-secondary rounded-3" id="filterButton"><i
-                                    class="bi bi-funnel-fill"></i></a>
-                        </div>
-
-                        <!-- Search Bar -->
-                        <div class="col-md-4">
-                            <form method="GET" action="#">
-
-                                <div class="input-group">
-                                    <input type="text" name="search" class="form-control"
-                                        placeholder="Cari nama dokter" aria-label="Cari" value="{{ request('search') }}"
-                                        aria-describedby="basic-addon1">
-                                    <button type="submit" class="btn btn-primary">Cari</button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="col-md-2">
-                            <a href="{{ route('permintaan-darah.create', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk]) }}"
-                                class="btn btn-primary">
-                                <i class="ti-plus"></i> Tambah
+        <div class="d-flex justify-content-center">
+            <div class="card w-100 h-100">
+                <div class="card-body">
+                    {{-- Tabs --}}
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a href="?tab=order" class="nav-link active" aria-selected="true">
+                                Order
                             </a>
-                        </div>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a href="?tab=monitoring" class="nav-link" aria-selected="true">
+                                Monitoring
+                            </a>
+                        </li>
+                    </ul>
 
-                    </div>
-                </div>
+                    {{-- Tab Content --}}
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active">
+                            {{-- TAB 1. buatlah list disini --}}
+                            <div class="row">
+                                <div class="d-flex justify-content-between m-3">
+                                    <div class="row">
+                                        <!-- Start Date -->
+                                        <div class="col-md-2">
+                                            <input type="date" name="start_date" id="start_date" class="form-control"
+                                                placeholder="Dari Tanggal">
+                                        </div>
 
-                <div class="table-responsive">
-                    <table class="table table-bordered table-sm table-hover">
-                        <thead class="table-primary">
-                            <tr>
-                                <th>No</th>
-                                <th>Tanggal Edukasi</th>
-                                <th>Petugas</th>
-                                <th>Kebutuhan Penerjemah</th>
-                                <th>Penerjemah Bahasa</th>
-                                <th>Tipe Pembelajaran</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {{-- @forelse($edukasi as $index => $item)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ date('d-m-Y H:i', strtotime($item->waktu_edukasi)) }}</td>
-                                    <td>{{ $item->userCreate->karyawan->gelar_depan .
-                                        ' ' .
-                                        str()->title($item->userCreate->karyawan->nama) .
-                                        ' ' .
-                                        $item->userCreate->karyawan->gelar_belakang ??
-                                        '-' }}
-                                    </td>
-                                    <td>
-                                        @if (isset($item->edukasiPasien))
-                                            {{ $item->edukasiPasien->kebutuhan_penerjemah == 1 ? 'Ya' : 'Tidak' }}
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if (isset($item->edukasiPasien) && $item->edukasiPasien->penerjemah_bahasa)
-                                            {{ $item->edukasiPasien->penerjemah_bahasa }}
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if (isset($item->edukasiPasien) && isset($item->edukasiPasien->tipe_pembelajaran_array))
-                                            {{ implode(', ', $item->edukasiPasien->tipe_pembelajaran_array) }}
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <div class="btn-group" role="group">
-                                            <a href="{{ route('edukasi.show', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, $item->id]) }}"
-                                                class="btn btn-info btn-sm" title="Detail">
-                                                <i class="ti-eye"></i>
-                                            </a>
-                                            <a href="{{ route('edukasi.edit', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, $item->id]) }}" class="btn btn-warning btn-sm ms-2" title="Edit">
-                                                <i class="ti-pencil"></i>
-                                            </a>
-                                            <form action="{{ route('edukasi.destroy', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, $item->id]) }}"
-                                                method="POST" class="delete-form" style="display: inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger btn-sm ms-2" title="Hapus">
-                                                    <i class="ti-trash"></i>
-                                                </button>
+                                        <!-- End Date -->
+                                        <div class="col-md-2">
+                                            <input type="date" name="end_date" id="end_date" class="form-control"
+                                                placeholder="S.d Tanggal">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <a href="#" class="btn btn-secondary rounded-3" id="filterButton"><i
+                                                    class="bi bi-funnel-fill"></i></a>
+                                        </div>
+
+                                        <!-- Search Bar -->
+                                        <div class="col-md-4">
+                                            <form method="GET" action="#">
+
+                                                <div class="input-group">
+                                                    <input type="text" name="search" class="form-control"
+                                                        placeholder="Cari nama dokter" aria-label="Cari"
+                                                        value="{{ request('search') }}" aria-describedby="basic-addon1">
+                                                    <button type="submit" class="btn btn-primary">Cari</button>
+                                                </div>
                                             </form>
                                         </div>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="8" class="text-center">Tidak ada data edukasi</td>
-                                </tr>
-                            @endforelse --}}
-                        </tbody>
-                    </table>
 
-                    <!-- Pagination -->
-                    <div class="d-flex justify-content-end">
-                        {{-- {{ $edukasi->links() }} --}}
+                                        <div class="col-md-2">
+                                            <a href="{{ route('permintaan-darah.create', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk]) }}"
+                                                class="btn btn-primary">
+                                                <i class="ti-plus"></i> Tambah
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="table-responsive">
+                                    <table class="table table-bordered table-sm table-hover">
+                                        <thead class="table-primary">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Tgl Pengiriman</th>
+                                                <th>Nama Petugas</th>
+                                                <th>Status</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse($permintaanDarah as $index => $item)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                
+                                                
+                                            </tr>
+                                            @empty
+                                            <tr>
+                                                <td colspan="8" class="text-center">Tidak ada data edukasi</td>
+                                            </tr>
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+
+                                    <!-- Pagination -->
+                                    <div class="d-flex justify-content-end">
+                                        {{ $permintaanDarah->links() }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
     </div>
+</div>
 @endsection
 
 
 @push('js')
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
             // Attach SweetAlert to all delete forms
             document.querySelectorAll('.delete-form').forEach(form => {
                 form.addEventListener('submit', function (e) {
@@ -166,5 +145,5 @@
                 });
             });
         });
-    </script>
+</script>
 @endpush
