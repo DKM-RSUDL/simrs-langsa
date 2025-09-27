348d171eeec45725eb478b7a26cd2da022c73b31
t-rizaldi <t.rizaldifadli2001@gmail.com>
2025-09-27 15:50:45 +0700

    menambah gate farmasi dan gizi

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,53 +29,53 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('access-unit', function ($user, $kd_unit) {
-            
-            if($user->hasRole('admin')) return true;
-            
+
+            if ($user->hasRole('admin')) return true;
+
             // jenis tenaga
             $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
             $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
             $kdUnitRuangan = $user->karyawan->ruangan->kd_unit;
 
             // DOKTER
-            if($kdJenisTenaga == 1) {
+            if ($kdJenisTenaga == 1) {
 
                 // Dokter Spesialis
                 // if(in_array($kdDetailJenisTenaga, [2,3])) {
-                    $dokter = Dokter::where('kd_karyawan', $user->kd_karyawan)->first();
-                    
-                    // get dokter unit RWJ
-                    $klinikList = DokterKlinik::where('kd_dokter', $dokter->kd_dokter)
-                                        ->pluck('kd_unit')
-                                        ->toArray();
+                $dokter = Dokter::where('kd_karyawan', $user->kd_karyawan)->first();
 
-                    // get dokter unit RWI
-                    $rwiList = DokterInap::where('kd_dokter', $dokter->kd_dokter)
-                                        ->pluck('kd_unit')
-                                        ->toArray();
+                // get dokter unit RWJ
+                $klinikList = DokterKlinik::where('kd_dokter', $dokter->kd_dokter)
+                    ->pluck('kd_unit')
+                    ->toArray();
 
-                    // dokter unit RWJ dan RWI
-                    $unitList = array_merge($klinikList, $rwiList);
+                // get dokter unit RWI
+                $rwiList = DokterInap::where('kd_dokter', $dokter->kd_dokter)
+                    ->pluck('kd_unit')
+                    ->toArray();
+
+                // dokter unit RWJ dan RWI
+                $unitList = array_merge($klinikList, $rwiList);
 
 
-                    return in_array($kd_unit, $unitList);
+                return in_array($kd_unit, $unitList);
 
                 // }
             }
 
             // PERAWAT
-            if($kdJenisTenaga == 2 && $kdDetailJenisTenaga == 1 && $kdUnitRuangan == $kd_unit) return true;
+            if ($kdJenisTenaga == 2 && $kdDetailJenisTenaga == 1 && $kdUnitRuangan == $kd_unit) return true;
             // BIDAN
-            if($kdJenisTenaga == 2 && $kdDetailJenisTenaga == 2 && $kdUnitRuangan == $kd_unit) return true;
+            if ($kdJenisTenaga == 2 && $kdDetailJenisTenaga == 2 && $kdUnitRuangan == $kd_unit) return true;
 
             return false;
         });
 
-        Gate::define('is-admin', function($user) {
+        Gate::define('is-admin', function ($user) {
             return $user->hasRole('admin');
         });
 
-        Gate::define('is-dokter-umum', function($user) {
+        Gate::define('is-dokter-umum', function ($user) {
             // jenis tenaga
             $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
             $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
@@ -83,17 +83,17 @@ class AuthServiceProvider extends ServiceProvider
             return $kdJenisTenaga == 1 && $kdDetailJenisTenaga == 1;
         });
 
-        Gate::define('is-dokter-spesialis', function($user) {
+        Gate::define('is-dokter-spesialis', function ($user) {
             // jenis tenaga
             $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
             $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
 
-            if($kdJenisTenaga == 1 && $kdDetailJenisTenaga == 1) return true;
-            if($kdJenisTenaga == 1 && $kdDetailJenisTenaga == 2) return true;
+            if ($kdJenisTenaga == 1 && $kdDetailJenisTenaga == 1) return true;
+            if ($kdJenisTenaga == 1 && $kdDetailJenisTenaga == 2) return true;
             return false;
         });
 
-        Gate::define('is-perawat', function($user) {
+        Gate::define('is-perawat', function ($user) {
             // jenis tenaga
             $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
             $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
@@ -101,12 +101,28 @@ class AuthServiceProvider extends ServiceProvider
             return $kdJenisTenaga == 2 && $kdDetailJenisTenaga == 1;
         });
 
-        Gate::define('is-bidan', function($user) {
+        Gate::define('is-bidan', function ($user) {
             // jenis tenaga
             $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
             $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
 
             return $kdJenisTenaga == 2 && $kdDetailJenisTenaga == 2;
+        });
+
+        Gate::define('is-farmasi', function ($user) {
+            // jenis tenaga
+            $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
+            $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
+
+            return $kdJenisTenaga == 3 && $kdDetailJenisTenaga == 2;
+        });
+
+        Gate::define('is-gizi', function ($user) {
+            // jenis tenaga
+            $kdJenisTenaga = $user->karyawan->kd_jenis_tenaga;
+            $kdDetailJenisTenaga = $user->karyawan->kd_detail_jenis_tenaga;
+
+            return $kdJenisTenaga == 3 && $kdDetailJenisTenaga == 5;
         });
     }
 }
