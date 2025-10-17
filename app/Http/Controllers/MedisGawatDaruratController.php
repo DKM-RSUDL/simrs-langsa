ef6e3bf58b88340978c79974cfdc07eb8216f65c
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2025-10-17 10:58:03 +0700

    feat[GAWAT DARURAT]: enhance patient triage functionality with modal for photo upload and data retrieval

--- app/Http/Controllers/MedisGawatDaruratController.php
+++ app/Http/Controllers/MedisGawatDaruratController.php
@@ -11,20 +11,27 @@ class MedisGawatDaruratController extends Controller
     public function index($kd_pasien, $tgl_masuk)
     {
         $dataMedis = Kunjungan::with(['pasien', 'dokter', 'customer', 'unit'])
-            ->where('kd_unit', 3)
-            ->where('kd_pasien', $kd_pasien)
-            ->whereDate('tgl_masuk', $tgl_masuk)
+            ->join('transaksi as t', function ($join) {
+                $join->on('kunjungan.kd_pasien', '=', 't.kd_pasien');
+                $join->on('kunjungan.kd_unit', '=', 't.kd_unit');
+                $join->on('kunjungan.tgl_masuk', '=', 't.tgl_transaksi');
+                $join->on('kunjungan.urut_masuk', '=', 't.urut_masuk');
+            })
+            ->select('kunjungan.*', 't.kd_kasir', 't.no_transaksi')
+            ->where('kunjungan.kd_unit', 3)
+            ->where('kunjungan.kd_pasien', $kd_pasien)
+            ->whereDate('kunjungan.tgl_masuk', $tgl_masuk)
             ->first();
+
+        if (!$dataMedis) {
+            abort(404, 'Data not found');
+        }
 
         // Menghitung umur berdasarkan tgl_lahir jika ada
         if ($dataMedis->pasien && $dataMedis->pasien->tgl_lahir) {
             $dataMedis->pasien->umur = Carbon::parse($dataMedis->pasien->tgl_lahir)->age;
         } else {
             $dataMedis->pasien->umur = 'Tidak Diketahui';
-        }
-
-        if (!$dataMedis) {
-            abort(404, 'Data not found');
         }
 
         return view('unit-pelayanan.gawat-darurat.action-gawat-darurat.index', compact('dataMedis'));
