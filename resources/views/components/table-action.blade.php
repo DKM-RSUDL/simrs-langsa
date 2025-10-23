ab0946c48cfcf779f488ba52c33c2e91d53762f6
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2025-10-23 16:14:24 +0700

    refactor[HEMODIALISA]: enhance UI and Code Structure for Tindakan Khusus and Traveling Dialysis Pages

--- resources/views/components/table-action.blade.php
+++ resources/views/components/table-action.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'd-flex gap-1 justify-content-center']) }}>
+<div {{ $attributes->merge(['class' => 'd-flex gap-1 justify-content-center flex-wrap']) }}>
     {{ $slot }}
 </div>
--- resources/views/unit-pelayanan/hemodialisa/pelayanan/data-umum/index.blade.php
+++ resources/views/unit-pelayanan/hemodialisa/pelayanan/data-umum/index.blade.php
@@ -11,91 +11,62 @@
 
         <div class="col-md-9">
             @include('components.navigation-hemodialisa')
-
-            <div class="d-flex justify-content-center">
-                <div class="card w-100 h-100">
-                    <div class="card-body">
-                        {{-- Tabs --}}
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <a href="#" class="nav-link active" aria-selected="true">Data Umum</a>
-                            </li>
-                        </ul>
-
-                        {{-- Tab Content --}}
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active">
-                                {{-- TAB 1. buatlah list disini --}}
-
-                                <div class="row">
-                                    <div class="d-flex justify-content-between align-items-center m-3">
-
-                                        <div class="row w-100">
-                                            <!-- Add Button -->
-                                            <!-- Include the modal file -->
-                                            <div class="col-md-2 ms-auto">
-                                                <div class="d-grid gap-2">
-                                                    <a href="{{ route('hemodialisa.pelayanan.data-umum.create', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk]) }}"
-                                                        class="btn btn-primary">
-                                                        <i class="ti-plus"></i> Tambah
-                                                    </a>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered table-sm table-hover">
-                                            <thead class="table-primary">
-                                                <tr align="middle">
-                                                    <th width="100px">NO</th>
-                                                    <th>TANGGAL</th>
-                                                    <th>PENANGGUNGJAWAB PASIEN</th>
-                                                    <th>PETUGAS</th>
-                                                    <th>AKSI</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @foreach ($dataUmum as $umum)
-                                                    <tr>
-                                                        <td align="middle">{{ $loop->iteration }}</td>
-                                                        <td align="middle">
-                                                            {{ date('Y-m-d H:i', strtotime($umum->created_at)) . ' WIB' }}
-                                                        </td>
-                                                        <td>{{ $umum->pj_nama }}</td>
-                                                        <td>
-                                                            {{ $umum->userCreate->karyawan->gelar_depan . ' ' . str()->title($umum->userCreate->karyawan->nama) . ' ' . $umum->userCreate->karyawan->gelar_belakang }}
-                                                        </td>
-                                                        <td align="middle">
-                                                            <a href="{{ route('hemodialisa.pelayanan.data-umum.show', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, encrypt($umum->id)]) }}"
-                                                                class="btn btn-sm btn-success">
-                                                                <i class="fas fa-eye"></i>
-                                                            </a>
-
-                                                            <a href="{{ route('hemodialisa.pelayanan.data-umum.edit', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, encrypt($umum->id)]) }}"
-                                                                class="btn btn-sm btn-warning mx-2">
-                                                                <i class="fas fa-edit"></i>
-                                                            </a>
-
-                                                            <button type="button"
-                                                                class="btn btn-sm btn-danger btn-del-data"
-                                                                data-umum="{{ encrypt($umum->id) }}"
-                                                                data-bs-target="#deleteModal">
-                                                                <i class="fas fa-trash"></i>
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <x-content-card>
+                @include('components.page-header', [
+                    'title' => 'Daftar Data Umum',
+                    'description' => 'Daftar data umum hemodialisa.',
+                ])
+                <div class="text-end">
+                    <a href="{{ route('hemodialisa.pelayanan.data-umum.create', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk]) }}"
+                        class="btn btn-primary">
+                        <i class="ti-plus"></i> Tambah
+                    </a>
                 </div>
-            </div>
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-sm table-hover">
+                        <thead class="table-primary">
+                            <tr align="middle">
+                                <th width="100px">NO</th>
+                                <th>TANGGAL</th>
+                                <th>PENANGGUNGJAWAB PASIEN</th>
+                                <th>PETUGAS</th>
+                                <th>AKSI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($dataUmum as $umum)
+                                <tr>
+                                    <td align="middle">{{ $loop->iteration }}</td>
+                                    <td align="middle">
+                                        {{ date('Y-m-d H:i', strtotime($umum->created_at)) . ' WIB' }}
+                                    </td>
+                                    <td>{{ $umum->pj_nama }}</td>
+                                    <td>
+                                        {{ $umum->userCreate->karyawan->gelar_depan . ' ' . str()->title($umum->userCreate->karyawan->nama) . ' ' . $umum->userCreate->karyawan->gelar_belakang }}
+                                    </td>
+                                    <td align="middle">
+                                        <a href="{{ route('hemodialisa.pelayanan.data-umum.show', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, encrypt($umum->id)]) }}"
+                                            class="btn btn-sm btn-success">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+
+                                        <a href="{{ route('hemodialisa.pelayanan.data-umum.edit', [$dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, encrypt($umum->id)]) }}"
+                                            class="btn btn-sm btn-warning mx-2">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+
+                                        <button type="button" class="btn btn-sm btn-danger btn-del-data"
+                                            data-umum="{{ encrypt($umum->id) }}" data-bs-target="#deleteModal">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </x-content-card>
         </div>
     </div>
 
