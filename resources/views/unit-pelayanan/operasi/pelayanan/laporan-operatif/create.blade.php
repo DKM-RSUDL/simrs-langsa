bad2a8985f2a51c4d3d4362de8a7ca2e0834c1c9
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2025-11-17 12:27:23 +0700

    feat[LAPORAN OPERASI]: update textarea placeholder and add rows for operation report description in create and edit views

--- resources/views/unit-pelayanan/operasi/pelayanan/laporan-operatif/create.blade.php
+++ resources/views/unit-pelayanan/operasi/pelayanan/laporan-operatif/create.blade.php
@@ -308,7 +308,7 @@
                         <div class="form-group">
                             <label for="laporan_prosedur_operasi" class="col-md-3 col-form-label"
                                 style="min-width: 200px;">Laporan Prosedur Operasi</label>
-                            <textarea placeholder="Jelaskan deskripsilengkap tentang prosedur bedah yang dilakukan "
+                            <textarea placeholder="Jelaskan deskripsilengkap tentang prosedur bedah yang dilakukan" rows="15"
                                 name="laporan_prosedur_operasi" id="laporan_prosedur_operasi" class="form-control"></textarea>
                         </div>
                     </div>
