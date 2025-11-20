1cf64485c87aefd3001c43df688ba8fe6be514af
TreatStart <rajartan201319@gmail.com>
2025-11-20 16:06:28 +0700

    refactor[LABOR RANAP]: remove unused Dokter model import from RawatInapLabPatologiKlinikController

--- app/Http/Controllers/UnitPelayanan/RawatInap/RawatInapLabPatologiKlinikController.php
+++ app/Http/Controllers/UnitPelayanan/RawatInap/RawatInapLabPatologiKlinikController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\UnitPelayanan\RawatInap;
 
 use App\Http\Controllers\Controller;
 use App\Models\AsalIGD;
-use App\Models\Dokter;
 use App\Models\DokterInap;
 use App\Models\Kunjungan;
 use App\Models\LapLisItemPemeriksaan;
@@ -21,7 +20,6 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class RawatInapLabPatologiKlinikController extends Controller
