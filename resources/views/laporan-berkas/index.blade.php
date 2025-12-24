ddb8a1cb9486b739f82dc428faf75a5e30a2329c
dewo1357 <dewo135789@gmail.com>
2025-12-24 13:16:56 +0700

    add fitur laporan-berkas

---
+++ resources/views/laporan-berkas/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.administrator.master')
+
+
+@section('content')
+    @php
+        $dataCard = [
+            [
+                "name_card" => "Master Berkas Digital",
+                "url" => "#"
+            ],
+            [
+                "name_card" => "Setting Berkas Claim",
+                "url" => "#"
+            ],
+            [
+                "name_card" => "Berkas Digital",
+                "url" => "#"
+            ],
+            [
+                "name_card" => "Setting Urutan Berkas Digital",
+                "url" => "#"
+            ]
+        ]
+    @endphp
+    <div class="row g-4">
+        @foreach ($dataCard as $item)
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <a href="{{ $item['url'] ?? '#' }}" class="text-decoration-none">
+                    <div class="card card-dashboard shadow-sm">
+                        <div class="card-body d-flex align-items-center gap-3">
+                            <div class="icon-box">
+                                <i class="{{ $item['icon'] ?? 'bi bi-folder2' }}"></i>
+                            </div>
+                            <div>
+                                <p class="mb-0 card-title">
+                                    {{ $item['name_card'] }}
+                                </p>
+                                <small class="text-muted">
+                                    {{ $item['subtitle'] ?? 'Kelola data' }}
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+
+
+
+
+@endsection
