e69da210cb52689630a8e8389cfc898310062239
t-rizaldi <t.rizaldifadli2001@gmail.com>
2026-01-01 10:05:10 +0700

    Enhance BerkasDigitalController and index view: Implement data tables for Rawat Inap and Rawat Jalan, improve profile display, and add filtering options for better user experience.

--- app/Http/Controllers/BerkasDigital/BerkasDigitalController.php
+++ app/Http/Controllers/BerkasDigital/BerkasDigitalController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\BerkasDigital;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Models\Kunjungan;
+use App\Models\Unit;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class BerkasDigitalController extends Controller
 {
@@ -16,8 +20,187 @@ class BerkasDigitalController extends Controller
         $this->pel = in_array($request->get('pel'), $this->pelArr) ? $request->get('pel') : 'ri';
     }
 
+
+    private function dataTableRanap($request)
+    {
+        $data = Kunjungan::with(['pasien', 'dokter', 'customer', 'unit'])
+            ->join('nginap', function ($q) {
+                $q->on('kunjungan.kd_pasien', 'nginap.kd_pasien');
+                $q->on('kunjungan.kd_unit', 'nginap.kd_unit');
+                $q->on('kunjungan.tgl_masuk', 'nginap.tgl_masuk');
+                $q->on('kunjungan.urut_masuk', 'nginap.urut_masuk');
+            })
+            ->join('transaksi as t', function ($q) {
+                $q->on('kunjungan.kd_pasien', '=', 't.kd_pasien');
+                $q->on('kunjungan.kd_unit', '=', 't.kd_unit');
+                $q->on('kunjungan.tgl_masuk', '=', 't.tgl_transaksi');
+                $q->on('kunjungan.urut_masuk', '=', 't.urut_masuk');
+            })
+            // ->where('nginap.kd_unit_kamar', $kd_unit)
+            ->where('nginap.akhir', 1)
+            ->where(function ($q) {
+                $q->whereNull('kunjungan.status_inap');
+                $q->orWhere('kunjungan.status_inap', 1);
+            })
+            ->whereNotNull('kunjungan.tgl_pulang')
+            ->whereNotNull('kunjungan.jam_pulang')
+            ->whereYear('kunjungan.tgl_masuk', '>=', 2025);
+
+        return DataTables::of($data)
+            ->filter(function ($query) use ($request) {
+                if ($searchValue = $request->get('search')['value']) {
+                    $query->where(function ($q) use ($searchValue) {
+                        if (is_numeric($searchValue) && strlen($searchValue) == 4) {
+                            $q->whereRaw("YEAR(kunjungan.tgl_masuk) = ?", [$searchValue]);
+                        } elseif (preg_match('/^\d{4}-\d{2}-\d{2}$/', $searchValue)) {
+                            $q->whereRaw("CONVERT(varchar, kunjungan.tgl_masuk, 23) like ?", ["%{$searchValue}%"]);
+                        } elseif (preg_match('/^\d{2}:\d{2}$/', $searchValue)) {
+                            $q->whereRaw("FORMAT(kunjungan.jam_masuk, 'HH:mm') like ?", ["%{$searchValue}%"]);
+                        } else {
+                            $normalized = str_replace('-', '', $searchValue);
+                            $q->whereRaw("REPLACE(kunjungan.kd_pasien, '-', '') like ?", ["%{$normalized}%"])
+                                ->orWhereHas('pasien', function ($q) use ($searchValue, $normalized) {
+                                    $q->whereRaw("REPLACE(kd_pasien, '-', '') like ?", ["%{$normalized}%"])
+                                        ->orWhere('nama', 'like', "%{$searchValue}%")
+                                        ->orWhere('alamat', 'like', "%{$searchValue}%");
+                                })
+                                ->orWhereHas('customer', function ($q) use ($searchValue) {
+                                    $q->where('customer', 'like', "%{$searchValue}%");
+                                });
+                        }
+                    });
+                }
+            })
+
+            ->order(function ($query) {
+                $query->orderBy('kunjungan.tgl_masuk', 'desc')
+                    ->orderBy('kunjungan.jam_masuk', 'desc');
+            })
+            ->editColumn('tgl_masuk', fn($row) => date('Y-m-d', strtotime($row->tgl_masuk)) ?: '-')
+            ->addColumn('no_rm', fn($row) => $row->kd_pasien ?: '-')
+            ->addColumn('alamat', fn($row) =>  $row->pasien->alamat ?: '-')
+            ->addColumn('jaminan', fn($row) =>  $row->customer->customer ?: '-')
+            // Hitung umur dari tabel pasien
+            ->addColumn('umur', function ($row) {
+                return hitungUmur($row->pasien->tgl_lahir);
+            })
+            ->addColumn('ruang', function ($row) {
+                $unit = Unit::where('kd_unit', $row->kd_unit_kamar)->first();
+                return !empty($unit) ? $unit->nama_unit : '-';
+            })
+            ->addColumn('profile', fn($row) => $row)
+            ->addColumn('action', function ($row) {
+                $payload = encrypt([
+                    'kd_kasir' => $row->kd_kasir,
+                    'no_transaksi' => $row->no_transaksi
+                ]);
+
+                return "<div class='text-center'>
+                                <a href='#' class='btn btn-primary mb-2'>
+                                    Lihat Data Klaim
+                                </a>
+
+                                <button class='btn btn-warning btnUnggahBerkas' data-ref='$payload'>
+                                    Unggah Berkas Pasien
+                                </button>
+                            </div>";
+            })
+            ->rawColumns(['action', 'profile'])
+            ->make(true);
+    }
+
+    private function dataTableRajal($request)
+    {
+        $data = Kunjungan::with(['pasien', 'dokter', 'customer', 'unit'])
+            ->join('transaksi as t', function ($join) {
+                $join->on('kunjungan.kd_pasien', '=', 't.kd_pasien');
+                $join->on('kunjungan.kd_unit', '=', 't.kd_unit');
+                $join->on('kunjungan.tgl_masuk', '=', 't.tgl_transaksi');
+                $join->on('kunjungan.urut_masuk', '=', 't.urut_masuk');
+            })
+            // ->where('kunjungan.kd_unit', $kd_unit)
+            // ->whereDate('kunjungan.tgl_masuk', '>=', now()->subDay()->format('Y-m-d'))
+            // ->whereDate('kunjungan.tgl_masuk', '<=', now()->endOfDay()->format('Y-m-d'))
+            ->whereYear('kunjungan.tgl_masuk', '>=', 2025)
+            ->where('t.Dilayani', 1);
+
+        return DataTables::of($data)
+            ->filter(function ($query) use ($request) {
+                if ($searchValue = $request->get('search')['value']) {
+                    $query->where(function ($q) use ($searchValue) {
+                        if (is_numeric($searchValue) && strlen($searchValue) == 4) {
+                            $q->whereRaw("YEAR(kunjungan.tgl_masuk) = ?", [$searchValue]);
+                        } elseif (preg_match('/^\d{4}-\d{2}-\d{2}$/', $searchValue)) {
+                            $q->whereRaw("CONVERT(varchar, kunjungan.tgl_masuk, 23) like ?", ["%{$searchValue}%"]);
+                        } elseif (preg_match('/^\d{2}:\d{2}$/', $searchValue)) {
+                            $q->whereRaw("FORMAT(kunjungan.jam_masuk, 'HH:mm') like ?", ["%{$searchValue}%"]);
+                        } else {
+                            $q->where('kunjungan.kd_pasien', 'like', "%{$searchValue}%")
+                                ->orWhereHas('pasien', function ($q) use ($searchValue) {
+                                    $q->where('nama', 'like', "%{$searchValue}%")
+                                        ->orWhere('alamat', 'like', "%{$searchValue}%");
+                                })
+                                ->orWhereHas('customer', function ($q) use ($searchValue) {
+                                    $q->where('customer', 'like', "%{$searchValue}%");
+                                });
+                        }
+                    });
+                }
+            })
+
+            ->order(function ($query) {
+                $query->orderBy('tgl_masuk', 'desc')
+                    ->orderBy('jam_masuk', 'desc');
+            })
+            ->editColumn('tgl_masuk', fn($row) => date('Y-m-d', strtotime($row->tgl_masuk)) ?: '-')
+            ->addColumn('no_rm', fn($row) => $row->kd_pasien ?: '-')
+            ->addColumn('alamat', fn($row) =>  $row->pasien->alamat ?: '-')
+            ->addColumn('jaminan', fn($row) =>  $row->customer->customer ?: '-')
+            // Hitung umur dari tabel pasien
+            ->addColumn('umur', function ($row) {
+                return hitungUmur($row->pasien->tgl_lahir);
+            })
+            ->addColumn('ruang', fn($row) =>  $row->unit->nama_unit ?: '-')
+            ->addColumn('profile', fn($row) => $row)
+            ->addColumn('action', function ($row) {
+                $payload = encrypt([
+                    'kd_kasir' => $row->kd_kasir,
+                    'no_transaksi' => $row->no_transaksi
+                ]);
+
+                return "<div class='text-center'>
+                                <a href='#' class='btn btn-primary mb-2'>
+                                    Lihat Data Klaim
+                                </a>
+
+                                <button class='btn btn-warning btnUnggahBerkas' data-ref='$payload'>
+                                    Unggah Berkas Pasien
+                                </button>
+                            </div>";
+            })
+            ->rawColumns(['action', 'profile'])
+            ->make(true);
+    }
+
     public function index(Request $request)
     {
-        return view('berkas-digital.document.index');
+        $kdBagian = $this->pel == 'rj' ? 2 : 1;
+        $unit = Unit::where('kd_bagian', $kdBagian)->where('aktif', 1)->get();
+        $customer = Customer::where('aktif', 1)->get();
+
+
+        if ($request->ajax()) {
+            if ($this->pel == 'ri') {
+                return $this->dataTableRanap($request);
+            }
+
+            if ($this->pel == 'rj') {
+                return $this->dataTableRajal($request);
+            }
+
+            return response()->json(['data' => []]);
+        }
+
+        return view('berkas-digital.document.index', compact('unit', 'customer'));
     }
 }
--- resources/views/berkas-digital/document/index.blade.php
+++ resources/views/berkas-digital/document/index.blade.php
@@ -39,12 +39,39 @@
             height: 2px;
             background-color: #3b82f6;
         }
+
+
+        /* Profile */
+        .profile {
+            display: flex;
+            align-items: center;
+        }
+
+        .profile img {
+            margin-right: 10px;
+            border-radius: 50%;
+        }
+
+        .profile .info {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .profile .info strong {
+            font-size: 14px;
+        }
+
+        .profile .info span {
+            font-size: 12px;
+            color: #777;
+        }
     </style>
 @endpush
 
 @section('content')
     @php
-        $tab = $_GET['pel'] ?? 'ri';
+        $tabArr = ['ri', 'rj']; // 'ri' for Rawat Inap, 'rj' for Rawat Jalan
+        $tab = in_array($_GET['pel'], $tabArr) ? $_GET['pel'] : 'ri';
     @endphp
 
     <x-content-card>
@@ -70,11 +97,14 @@
         <div class="row">
             <div class="col">
                 <div class="row mb-3">
-                    <div class="col-md-4">
+                    <div class="col-md-3">
                         <div class="form-group">
                             <label class="form-label" for="unit_filter">{{ $tab == 'rj' ? 'Poli' : 'Ruang' }}</label>
                             <select class="form-select select2" id="unit_filter">
-                                <option value="">--Pilih Kamar--</option>
+                                <option value="">--Pilih Ruang--</option>
+                                @foreach ($unit as $item)
+                                    <option value="{{ $item->kd_unit }}">{{ $item->nama_unit }}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
@@ -84,11 +114,25 @@
                             <label for="customer_filter" class="form-label">Jenis Bayar</label>
                             <select id="customer_filter" class="form-select select2">
                                 <option value="">--Pilih Jenis Bayar--</option>
+                                @foreach ($customer as $item)
+                                    <option value="{{ $item->kd_customer }}">{{ $item->customer }}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
 
-                    <div class="col-md-5">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="status_filter" class="form-label">Status Klaim</label>
+                            <select id="status_filter" class="form-select select2">
+                                <option value="">--Pilih Status Klaim--</option>
+                                <option value="0">Belum Selesai</option>
+                                <option value="1">Selesai</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
                         <label for="periode_filter" class="form-label">Periode</label>
                         <div class="input-group input-daterange">
                             <input type="text" class="form-control" id="startdate_filter" readonly
@@ -104,13 +148,13 @@
                     <table class="table table-bordered" id="dataTable">
                         <thead>
                             <tr>
-                                <th style="width:5%">Aksi</th>
                                 <th>Pasien</th>
                                 <th>No RM / Tgl Masuk</th>
+                                <th>{{ $tab == 'rj' ? 'Poli' : 'Ruang' }}</th>
                                 <th>DPJP</th>
                                 <th>Alamat</th>
                                 <th>Jaminan</th>
-                                <th>Status Pelayanan</th>
+                                <th>Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -127,13 +171,96 @@
 @push('js')
     <script>
         $(document).ready(function() {
-            $('#dataTable').DataTable();
 
             $('.input-daterange input').each(function() {
                 $(this).datepicker({
                     format: 'yyyy-mm-dd',
                 });
             });
+
+
+            // DATATABLE
+            $('#dataTable').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('berkas-digital.dokumen.index') }}?pel={{ $tab }}",
+                columns: [{
+                        data: 'profile',
+                        name: 'profile',
+                        render: function(data, type, row) {
+                            let imageUrl = row.foto_pasien ?
+                                "{{ asset('storage/') }}" + '/' + row.foto_pasien :
+                                "{{ asset('assets/images/avatar1.png') }}";
+                            let gender = row.pasien.jenis_kelamin == '1' ? 'Laki-Laki' :
+                                'Perempuan';
+                            return `
+                                <div class="profile">
+                                    <img src="${imageUrl}" alt="Profile" width="50" height="50" class="rounded-circle"/>
+                                    <div class="info">
+                                        <strong>${row.pasien.nama}</strong>
+                                        <span>${gender} / ${row.umur} Tahun</span>
+                                    </div>
+                                </div>
+                            `;
+                        },
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'kd_pasien',
+                        name: 'kd_pasien',
+                        render: function(data, type, row) {
+                            return `
+                                <div class="rm-reg">
+                                    RM: ${row.kd_pasien ? row.kd_pasien : 'N/A'}<br>
+                                    Tgl: ${row.tgl_masuk ? row.tgl_masuk : 'N/A'}
+                                </div>
+                            `;
+                        },
+                        defaultContent: ''
+                    },
+                    {
+                        data: 'ruang',
+                        name: '',
+                        defaultContent: ''
+                    },
+                    {
+                        data: 'dokter.nama_lengkap',
+                        name: '',
+                        defaultContent: ''
+                    },
+                    {
+                        data: 'alamat',
+                        name: 'alamat',
+                        defaultContent: ''
+                    },
+                    {
+                        data: 'jaminan',
+                        name: 'jaminan',
+                        defaultContent: ''
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ],
+                paging: true,
+                lengthChange: true,
+                searching: true,
+                ordering: true,
+                info: true,
+                autoWidth: false,
+                responsive: true,
+            });
+        });
+
+
+        $('#dataTable').on('click', '.btnUnggahBerkas', function() {
+            let $this = $(this);
+            let ref = $this.attr('data-ref');
+            console.log(ref);
         });
     </script>
 @endpush
