96dcdae28d48e40902a05d64bf0e61af7cd526b8
dewo1357 <dewo135789@gmail.com>
2026-01-09 19:27:25 +0700

    Add Print Opthamology

--- resources/views/unit-pelayanan/rawat-inap/pelayanan/asesmen/index-asesmenawal.blade.php
+++ resources/views/unit-pelayanan/rawat-inap/pelayanan/asesmen/index-asesmenawal.blade.php
@@ -299,6 +299,9 @@
                         class="btn btn-sm btn-info">
                         <i class="fas fa-eye me-1"></i> Lihat
                     </a>
+                      <a id="btnPrintAsesmen" href="{{ route('rawat-inap.asesmen.keperawatan.opthamology.print', [$dataMedis->kd_unit, $dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, $item->id]) }}" target="_blank" class="btn btn-primary btn-sm">
+                        <i class="bi bi-printer"></i> Print
+                    </a>
 
                     <a href="{{ route('rawat-inap.asesmen.keperawatan.opthamology.edit', [$dataMedis->kd_unit, $dataMedis->kd_pasien, date('Y-m-d', strtotime($dataMedis->tgl_masuk)), $dataMedis->urut_masuk, $item->id]) }}"
                         class="btn btn-sm btn-secondary">
